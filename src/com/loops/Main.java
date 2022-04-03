9c9bffcec9c6a360654159e4af9691a9456afb9c
Renan_Fonseca <renanfonseca88@gmail.com>
2022-04-03 01:11:30 -0300

    Adicionando codigo principal

--- src/com/loops/Main.java
+++ src/com/loops/Main.java
@@ -1,9 +1,30 @@
 package com.loops;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
 
+        int i = 0;
+        int numero = 0;
+        int maior = 0;
+        while(i < 5){
+            System.out.println("Digite um numero ");
+
+            numero = entrada.nextInt();
+
+            if(numero > maior){
+                maior = numero;
+            }
+
+
+
+            i++;
+
+        }
+        System.out.println("O maior numero é "+maior);
     }
 }
